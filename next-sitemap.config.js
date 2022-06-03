6e46e01fa485ef9c60e76eaf0cf6d0969ae42bee
dikdigguh <cdan198@gmail.com>
2022-06-03 16:21:48 -0700

    sitem commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: process.env.SITE_URL || 'https://fingerboardweekly.com',
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 7000,
+    // ...other options
+    }
+    
+    export default config
+    
