5a659abd5c548cd71ddca669b233feb2688d89e7
dikdigguh <cdan198@gmail.com>
2022-06-03 08:03:23 -0700

    seven commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Layout } from '../components';
+import Script from 'next/script';
 
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { useEffect, useState } from 'react'
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <React.Fragment>
+  <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  
+    </React.Fragment>
   )
 }
 
