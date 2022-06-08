c19bb7a004770bf4dff3b85d94ea8d57a0498a99
dikdigguh <cdan198@gmail.com>
2022-06-08 13:58:06 -0700

    fix category post order

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,14 +16,15 @@ const Header = () => {
     <div className="border-b w-full inline-block border-gray-400 py-8">
       <div className="md:float-left block">
         <Link href="/">
-          <span className="cursor-pointer text-4xl text-white">FINGERBOARD WEEKLY</span>
+          <span className="cursor-pointer text-4xl text-dark">FINGERBOARD WEEKLY</span>
         </Link>
       </div>
       <div className="hidden md:float-left md:contents">
         {categories.map((category, index) => (
-          <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-dark ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
         ))}
       </div>
+
     </div>
   </div>
    );
